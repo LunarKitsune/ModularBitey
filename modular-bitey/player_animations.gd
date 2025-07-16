70d9ef5c1664173694b996d7eb7170f43c2039e7
Cheesypockets <47466235+LunarKitsune@users.noreply.github.com>
2025-07-15 22:36:05 -0400

    Update player_animations.gd
    -Updated by having a function that can take an animation name to simplify calling the change animation
    -Made a queue animation for the same thing
    -Queued idle animation after bite animation in play_bite_animation so certain conditions in player.gd can fire properly

--- modular-bitey/player_animations.gd
+++ modular-bitey/player_animations.gd
@@ -1,5 +1,22 @@
 extends Node2D
 
+#note:
+#original signal picked for _animation_finished was _on_animation_player_animation_finished
+#Changing signal to blank in Node on animation player under signals tab in _aniamtion finished.
+#since the connection happens in the _ready() function of this class. 
+
+#new note: Fixed the animation issue by queuing the idle animation in the same function
+#for playing the bite animation
+
+#made a flexible play_animation function that will take an animation parameter
+#this way for later any additional animations needed the name can be thrown in
+#without having to directly call the animation player. 
+
+#same for queue animation so there is no need for searching the method through
+#animation player. 
+
+#curr
+
 
 # variables
 var current_animation := "" # for animation helper function
@@ -7,47 +24,50 @@ var this_node = ""
 
 func _ready() -> void:
 	# return to other stuff
-	%AnimationPlayer.connect("animation_finished", Callable(self, "_on_animation_finished"))
+	%AnimationPlayer.connect("animation_finished", Callable(self, "_on_animation_finished"));
 	# set this_node to node's name
 	this_node == name
 	
 
 # ANIMATION HELPER (not sure if working yet?)
-func _on_animation_finished(anim_name):
-	if current_animation == anim_name:
-		current_animation = ""
+#func _on_animation_finished(anim_name):
+	#if current_animation == anim_name:
+		#current_animation = "";
 
 
 # HELPER FUNCTION:  play animation only if different
-func change_animation(anim_name: String):
-	if current_animation != anim_name: 
-		%AnimationPlayer.play(anim_name)
-		current_animation = anim_name
+#func change_animation(anim_name: String):
+	#%AnimationPlayer.play(anim_name);
+	#if current_animation != anim_name:
+	#	current_animation = anim_name;
 
-func clear_and_queue_animation(anim_name: String):
-	if current_animation != anim_name: 
-		%AnimationPlayer.clear_queue()
-		%AnimationPlayer.queue(anim_name)
-		current_animation = anim_name
+func play_animation(animationName:String):
+	%AnimationPlayer.play(animationName);
 
 func play_idle_animation(): 
 	# %AnimationPlayer.play("idle")
-	change_animation("idle1")
-
+	play_animation("idle1");
 
 func play_walk_animation():
 	# %AnimationPlayer.play("walk")
-	change_animation("run2")
+	play_animation("run2");
+	
 
 func play_bite_animation(): #NOT CURRENTLY WORKING! fix it later
 	# %AnimationPlayer.play("bite")
-	# change_animation("bite")
-	clear_and_queue_animation("bite")
-	print_debug("Bite!")
+	play_animation("bite");
+	queue_animation("idle1");
+	print_debug("Bite!");
 	
-	
+func queue_animation(animationName:String):
+	%AnimationPlayer.queue(animationName);
+
 func play_test_animation():
 	#%AnimationPlayer.play("TEST")
-	change_animation("TEST")
+	play_animation("TEST");
 	
- #func _on_animation_finished(anim_name):
+
+
+#func _on_animation_player_animation_finished(anim_name):
+#	pass # Replace with function body.
+
